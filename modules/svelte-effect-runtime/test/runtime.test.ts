f4f8672e40f3cf858fcead871225b1e6156f8c84
ss <sqndxr@gmail.com>
2026-04-16 18:10:33 +0200

    Fix runtime test lint import

--- modules/svelte-effect-runtime/test/runtime.test.ts
+++ modules/svelte-effect-runtime/test/runtime.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertRejects, assertStringIncludes } from "@std/assert";
+import { assertEquals, assertStringIncludes } from "@std/assert";
 import * as Context from "effect/Context";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
